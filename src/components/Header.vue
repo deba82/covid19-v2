298bd995b0b5367a642506a0e27271bda2fa4e0d
debasish <debasish.earth@gmail.com>
2021-06-02 01:33:49 +0530

    Header made

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,58 @@
 <template>
-  <div>This is header</div>
+  <div>
+    <h1>
+      Covid 19-Tracker
+      <i class="fa i-virus">
+        &#xe074;
+      </i>
+    </h1>
+    <p>
+      <a href="#"
+        >!!! <i class="fa i-smiley"> &#xf118; </i> Source from Covid-19 API
+        <i class="fa i-smiley"> &#xf118; </i> !!!</a
+      >
+    </p>
+  </div>
 </template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Mate+SC&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Padauk&display=swap");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css");
+* {
+  color: black;
+  text-decoration: none;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
+<style scoped>
+h1 {
+  text-align: center;
+  background: chocolate;
+  color: cornsilk;
+  margin: 5px auto;
+  padding: 5px;
+  font-family: "Mate SC", serif;
+}
+p > a,
+p {
+  text-align: center;
+  font-size: 1rem;
+  color: blue;
+  font-weight: 1000;
+  border-bottom: 3px solid burlywood;
+  font-family: "Padauk", sans-serif;
+}
+.i-smiley {
+  color: blue;
+}
+.i-virus {
+  color: cornsilk;
+}
+</style>
